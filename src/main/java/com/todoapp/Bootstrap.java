3d0515f50ea79d3edb408309a9d6eb7be0e69711
ronak <ronak@rkmallik.com>
2014-07-01 13:33:45 +1000

    merging @shekhargulati's two repos: the app src and the openshift hooks, plus changes to get maven build to work

---
+++ src/main/java/com/todoapp/Bootstrap.java
@@ -0,0 +1,43 @@
+package com.todoapp;
+
+import com.mongodb.*;
+
+import static spark.Spark.setIpAddress;
+import static spark.Spark.setPort;
+import static spark.SparkBase.staticFileLocation;
+
+/**
+ * Created by shekhargulati on 09/06/14.
+ */
+public class Bootstrap {
+    private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
+    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
+
+    public static void main(String[] args) throws Exception {
+        setIpAddress(IP_ADDRESS);
+        setPort(PORT);
+        staticFileLocation("/public");
+        new TodoResource(new TodoService(mongo()));
+    }
+
+    private static DB mongo() throws Exception {
+        String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+        if (host == null) {
+            MongoClient mongoClient = new MongoClient("localhost");
+            return mongoClient.getDB("todoapp");
+        }
+        int port = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+        String dbname = System.getenv("OPENSHIFT_APP_NAME");
+        String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+        MongoClientOptions mongoClientOptions = MongoClientOptions.builder().connectionsPerHost(20).build();
+        MongoClient mongoClient = new MongoClient(new ServerAddress(host, port), mongoClientOptions);
+        mongoClient.setWriteConcern(WriteConcern.SAFE);
+        DB db = mongoClient.getDB(dbname);
+        if (db.authenticate(username, password.toCharArray())) {
+            return db;
+        } else {
+            throw new RuntimeException("Not able to authenticate with MongoDB");
+        }
+    }
+}
